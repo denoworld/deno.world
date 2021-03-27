ae05ca0419c826cdcfd7c25aa99332817aaa5992
Satya Rohith <me@satyarohith.com>
2021-03-27 21:44:04 +0530

    fix import

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,7 +1,7 @@
 import { h } from "../deps.ts";
 import Layout from "../components/layout.jsx";
 import Posts from "../components/posts.jsx";
-import { meta as mockHttpRequests } from "./2021/03/24/mock-http-requests.jsx";
+import { meta as mockHttpRequests } from "./2021/03/28/mock-http-requests.jsx";
 
 export default async function Home() {
   return <Layout title="Home">
